f70598b11a82cc368eb255aab82c59c6636cbb01
wenhaijing <xwenhaha@163.com>
2022-12-14 15:39:00 +0800

    feat: 新增打包配置文件

--- build/js/776.9a4c1146.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkvue3_ts_cms"]=self["webpackChunkvue3_ts_cms"]||[]).push([[776],{57776:function(n,u,e){e.r(u),e.d(u,{default:function(){return i}});var r=e(73396);function t(n,u,e,t,s,c){return(0,r.wg)(),(0,r.iD)("div",null,"Login")}var s=(0,r.aZ)({setup(){return{}}}),c=e(40089);const a=(0,c.Z)(s,[["render",t]]);var i=a}}]);
-//# sourceMappingURL=776.9a4c1146.js.map
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 const path = require('path')
+const BundleAnalyzerPlugin =
+  require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 module.exports = defineConfig({
   outputDir: './build',
   publicPath: './',
@@ -20,5 +22,24 @@ module.exports = defineConfig({
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src'))
       .set('components', '@/components')
+  },
+  configureWebpack: (config) => {
+    const plugins = []
+    // 配置BundleAnalyzerPlugin
+    plugins.push(
+      new BundleAnalyzerPlugin({
+        analyzerMode: 'server',
+        analyzerHost: '127.0.0.1',
+        analyzerPort: 8888, // 运行后的端口号
+        reportFilename: 'report.html',
+        defaultSizes: 'parsed',
+        openAnalyzer: true,
+        generateStatsFile: false,
+        statsFilename: 'stats.json',
+        statsOptions: null,
+        logLevel: 'info'
+      })
+    )
+    return { plugins }
   }
 })
